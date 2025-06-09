193bbf10470c6a6f1b914db90bf08928b4c963c5
VeronikaVS90 <vsokolovanika@gmail.com>
2025-06-09 13:45:19 +0300

    did task1

---
+++ jsBasics/task1.js
@@ -0,0 +1,19 @@
+// string
+let testString = "I'm doing my homework";
+console.log(testString);
+
+// number
+let testNumber = 4;
+console.log(testNumber);
+
+// boolean
+let testBoolean = true;
+console.log(testBoolean);
+
+// null
+let testNull = null;
+console.log(testNull);
+
+// undefined (by default)
+let testUndefined;
+console.log(testUndefined);
