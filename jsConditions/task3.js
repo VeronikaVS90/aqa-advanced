2588f57449fae755a8be24216788ca159b1fe88c
VeronikaVS90 <vsokolovanika@gmail.com>
2025-06-14 23:38:12 +0300

    completed task3

---
+++ jsConditions/task3.js
@@ -0,0 +1,13 @@
+// for
+const number = 3;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} * ${i} = ${number * i} `);
+};
+
+// while
+let i = 1;
+while (i <= 10) {
+    console.log(`${number} * ${i} = ${number * i} `);
+    i++;
+};
