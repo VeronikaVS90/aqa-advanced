7f32e513e1853ec1c0ed53a5d9e5f6fe5977bd1d
VeronikaVS90 <vsokolovanika@gmail.com>
2025-06-16 22:06:37 +0300

    fixed task1

--- jsConditions/task1.js
+++ jsConditions/task1.js
@@ -2,11 +2,11 @@ const averageGrade = 75;
 
 if (averageGrade < 60) {
     console.log("Незадовільно");
-} else if (averageGrade >= 60 && averageGrade <= 70) {
+} else if (averageGrade <= 70) {
     console.log("Задовільно");
-} else if (averageGrade >= 71 && averageGrade <= 80) {
+} else if (averageGrade <= 80) {
     console.log("Добре");
-} else if (averageGrade >= 81 && averageGrade <= 90) {
+} else if (averageGrade <= 90) {
     console.log("Дуже добре");
 } else {
     console.log("Відмінно");
