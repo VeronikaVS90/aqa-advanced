714686c14761c62eeb2c558e486b1b52a3a35a33
VeronikaVS90 <vsokolovanika@gmail.com>
2025-06-18 22:47:34 +0300

    did task3

---
+++ jsFunctions/task3.js
@@ -0,0 +1,9 @@
+const checkOrder = (available, ordered) => {
+    if (ordered === 0) {
+        return "Your order is empty";
+    } else if (ordered > available) {
+        return "Your order is too large, we don't have enough goods.";
+    } else {
+        return "Your order is accepted";
+    }
+};
