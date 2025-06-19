3f3d2c3edad09b9e2ffaca35aeddd9ec58b7d51f
VeronikaVS90 <vsokolovanika@gmail.com>
2025-06-19 19:25:25 +0300

    fixed task3 (early return)

--- jsFunctions/task3.js
+++ jsFunctions/task3.js
@@ -1,9 +1,11 @@
 const checkOrder = (available, ordered) => {
     if (ordered === 0) {
         return "Your order is empty";
-    } else if (ordered > available) {
-        return "Your order is too large, we don't have enough goods.";
-    } else {
-        return "Your order is accepted";
     }
+    
+    if (ordered > available) {
+        return "Your order is too large, we don't have enough goods.";
+    }
+    
+    return "Your order is accepted";
 };
