f2ff25d143dc69bf8697244041ecb822c02e8884
Veronika Vitishchenko-Sokolova <117116182+VeronikaVS90@users.noreply.github.com>
2025-06-26 21:57:49 +0300

    Update jsArrays/task3.js
    Co-authored-by: Kwazimodd <55921352+Kwazimodd@users.noreply.github.com>

--- jsArrays/task3.js
+++ jsArrays/task3.js
@@ -1,6 +1,6 @@
 const numbers = [10, 20, 30, 40, 50];
 
-const sum = numbers.reduce((accumulator, currentValue) => {
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue)
     return accumulator + currentValue;
 }, 0);
 
