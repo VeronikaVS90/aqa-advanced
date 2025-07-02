fdc4ce2f10e90bd0a12204a9f3c3b84d65301076
VeronikaVS90 <vsokolovanika@gmail.com>
2025-07-02 22:46:50 +0300

    did homework10

---
+++ jsOOP/Book.js
@@ -0,0 +1,52 @@
+class Book {
+	constructor(title, author, year) {
+		this._title = title;
+		this._author = author;
+		this._year = year;
+	}
+
+	get title() {
+		return this._title;
+	}
+	set title(value) {
+		if (typeof value !== 'string' || value.trim() === '') {
+			throw new Error('Invalid title');
+		}
+		this._title = value;
+	}
+
+	get author() {
+		return this._author;
+	}
+	set author(value) {
+		if (typeof value !== 'string' || value.trim() === '') {
+			throw new Error('Invalid author');
+		}
+		this._author = value;
+	}
+
+	get year() {
+		return this._year;
+	}
+	set year(value) {
+		if (typeof value !== 'number' || value < 0) {
+			throw new Error('Invalid year');
+		}
+		this._year = value;
+	}
+
+	printInfo() {
+		console.log(`Title: ${this.title}, Author: ${this.author}, Year: ${this.year}`);
+	}
+
+	static getOldestBook(books) {
+		if (!Array.isArray(books) || books.length === 0) {
+			return null;
+		}
+		return books.reduce((oldest, current) =>
+			current.year < oldest.year ? current : oldest
+		);
+	}
+}
+
+export default Book;
---
+++ jsOOP/EBook.js
@@ -0,0 +1,31 @@
+import Book from "./Book";
+
+class EBook extends Book {
+	constructor(title, author, year, fileFormat) {
+		super(title, author, year);
+		this._fileFormat = fileFormat;
+	}
+
+	get fileFormat() {
+		return this._fileFormat;
+	}
+	set fileFormat(value) {
+		if (typeof value !== 'string' || value.trim() === '') {
+			throw new Error('Invalid file format');
+		}
+		this._fileFormat = value;
+	}
+
+	printInfo() {
+		console.log(`Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Format: ${this.fileFormat}`);
+	}
+
+	static createFromBook(book, format) {
+		if (!(book instanceof Book)) {
+			throw new Error('Argument must be an instance of Book');
+		}
+		return new EBook(book.title, book.author, book.year, format);
+	}
+}
+
+export default EBook;
